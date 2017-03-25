d654b1bf6354ca8d2eb10a79fc1da8b1eec6c9e0
ashvinee <ashvinee@btech.christuniversity.in>
2017-03-25 19:16:06 +0530

    project on ar

---
+++ project_2/Exclusive Images/Assets/RotatingItem2.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingItem2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(Time.deltaTime*500, 0, 0));
+	
+	}
+}
